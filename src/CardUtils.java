f8f1c683a78874bb2cf8b253747d97055c03605f
ASMAEELKHIRAOUI <asmaaekw@gmail.com>
2023-09-22 10:54:00 +0100

    refactored the create cards method

--- src/CardUtils.java
+++ src/CardUtils.java
@@ -1,41 +1,55 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class CardUtils {
-    public static List<int[]> createDeckOfCards() {
-        List<int[]> deckOfCards = new ArrayList<int[]>();
+    public static int[][] createDeckOfCards(int startingValue, int startingSuit) {
+        int[][] deck = new int[52][2];
+        int index = 0;
 
-        for (int suit = 1; suit <= 4; suit++) {
-            for (int value = 1; value <= 13; value++) {
-                deckOfCards.add(new int[]{value, suit});
+        // Create cards from the starting card to the end
+        for (int suit = startingSuit; suit <= 4; suit++) {
+            for (int value = (suit == startingSuit ? startingValue : 1); value <= 13; value++) {
+                deck[index][0] = value;
+                deck[index][1] = suit;
+                index++;
             }
         }
 
-        for (int[] card : deckOfCards) {
+        // Create cards from the beginning to the starting card
+        for (int suit = 1; suit < startingSuit+1; suit++) {
+            for (int value = 1; value < (suit==startingSuit?startingValue:14); value++) {
+                deck[index][0] = value;
+                deck[index][1] = suit;
+                index++;
+            }
+        }
+
+        return deck;
+    }
+
+
+    public static String getCardName(int value, int suit) {
+        return value + "-" + suit;
+    }
+
+    public static void show(int startingValue, int startingSuit) {
+        int[][] deck = createDeckOfCards(startingValue, startingSuit);
+
+        for (int[] card : deck) {
             int value = card[0];
             int suit = card[1];
             printColoredCard(getCardName(value, suit), suit);
         }
-
-        return deckOfCards;
-    }
-
-    public static String getCardName(int value, int suit) {
-        String[] suits = {"♥️", "♦️", "♣️", "♠️"};
-        String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-        return values[value - 1] + suits[suit - 1];
     }
 
     public static void printColoredCard(String cardName, int suit) {
         String colorCode;
         if (suit == 1 || suit == 2) {
-            colorCode = "\u001B[31m";
+            colorCode = "\u001B[31m"; // Red color for Diamonds and Hearts
         } else {
-            colorCode = "\u001B[35m";
+            colorCode = "\u001B[35m"; // Purple color for Spades and Clubs
         }
 
         String resetCode = "\u001B[0m";
 
         System.out.println(colorCode + cardName + resetCode);
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        CardUtils.createDeckOfCards();
+        CardUtils.show(12,4);
     }
 }
